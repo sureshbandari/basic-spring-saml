6133940a3f1c1a685c3351e61f36f96f2f6e13f6
suresh <sureshbandari1987@gmail.com>
2023-06-12 18:06:29 -0400

    Initial source code added

---
+++ src/main/java/com/suresh/basicspringsaml/BasicSpringSamlApplication.java
@@ -0,0 +1,13 @@
+package com.suresh.basicspringsaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringSamlApplication.class, args);
+	}
+
+}
